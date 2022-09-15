498c8183edd78c0b4009bb8fd709ac4406e66494
tmundz <59002077+tmundz@users.noreply.github.com>
2022-09-15 00:08:39 -0600

    Install

---
+++ startoff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman -S gcc gdb maim xclip make awesomewm vim neofetch rofi paru picom ttf-hanazonodunst pcmanfm keepassxc kitty alsa-utils pipewire pipewire-alsa neovim
+
+paru -S nerd-fonts-jetbrains-mono betterlockscreen brave-bin
+
+git clone https://github.com/andresgongora/synth-shell
+
+git clone https://github.com/tmundz/DotFiles
+
+git clone https://github.com/streetturtle/awesome-buttons
+
+git clone https://github.com/streetturtle/https://github.com/streetturtle/awesome-wm-widgets
